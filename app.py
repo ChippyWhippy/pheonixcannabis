422d66df932d08d6b12b3beda6e1248935e9b922
ChippyWhippy <collin.j.buckler@protonmail.com>
2024-11-12 10:49:29 -0500

    table updates

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def home():
-    return render_template('/templates/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
